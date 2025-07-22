d327328456593142708783c2538f61a16348594d
Sumit Saurabh <sumitsaurabh98@gmail.com>
2025-07-22 10:24:42 +0530

    qa, np, pd

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ module "qa" {
     app_name      = "qa-blog"
     network_name  = "qa"
 }
+
+module "np" {
+    source = "./modules/blog"
+
+    project_id    = var.project_id
+    region        = var.region
+
+    app_name      = "np-blog"
+    network_name  = "np"
+}
+
+module "pd" {
+    source = "./modules/blog"
+
+    project_id    = var.project_id
+    region        = var.region
+
+    app_name      = "pd-blog"
+    network_name  = "pd"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "qa_ip" {
     value	= module.qa.public_ip
 }
+
+output "np_ip" {
+    value	= module.np.public_ip
+}
+
+output "pd_ip" {
+    value	= module.pd.public_ip
+}
